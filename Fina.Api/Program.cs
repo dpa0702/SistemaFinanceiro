693da1e65c1d550a7dcd7b4f488465061a67201d
demetrios arghirachis <dpa0702@gmail.com>
2024-05-31 15:55:16 -0300

    Publicação da api em imagem docker

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -10,7 +10,7 @@ builder.AddDocumentation();
 builder.AddServices();
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
     app.ConfigureDevEnvironment();
 
 app.UseCors(ApiConfiguration.CorsPolicyName);
